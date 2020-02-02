f6359a4570af16409041d58dfc792e4728c9d225
Jacob Majcan <jacob.majcan16@gmail.com>
2020-02-02 12:59:12 -0400

    AddingLab2

---
+++ Lab2/Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ghost;
+    public Text timerText;
+    public Text gameEndText;
+    private int timer = 0;
+    private bool gameEnd = false;
+    int i = 5;
+
+    // Start is called before the first frame update
+    private void Start() {
+        SpawnGhost();
+        gameEndText.text = ""; // Set End Game Text to nothing
+        SetTimerText();
+    }
+
+    private void Update() {
+        SetTimerText();
+
+         // Game has ended
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            // Exit Game
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            // Restart Game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if ((int)Time.time == (i-1) && timer != 0 && gameEnd != true) {
+            i += 5;
+            SpawnGhost();
+        }
+    }
+
+    private void SpawnGhost() {
+        Instantiate(ghost, new Vector3(0f, 0f, 0f), Quaternion.identity);
+    }
+
+    private void SetTimerText() {
+        timerText.text = "Timer: " + timer.ToString();
+ 
+        if (gameEnd == true) {
+            timer += (int)Time.deltaTime; // pause time
+        } else {
+            timer = (int)Time.timeSinceLevelLoad + 1;
+        }
+    }
+
+    public void GameOver() {
+        timer = (int)Time.timeSinceLevelLoad;
+        gameEnd = true;
+        gameEndText.text = "GAME OVER\nPress ESC to exit\nPress R to restart.";
+    }
+}
---
+++ Lab2/Assets/Scripts/GhostController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GhostController : MonoBehaviour
+{
+    private Vector3 velocity;
+
+    private SpriteRenderer rend;
+    private Animator anim;
+    public float speed = 2.0f;
+
+    // Use this for initialization
+    void Start() {
+        velocity = new Vector3(-1f, 0f, 0f);
+        rend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        anim.Play("GhostLeft");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // calculate location of screen borders
+        // this will make more sense after we discuss vectors and 3D
+        var dist = (transform.position - Camera.main.transform.position).z;
+        var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+
+        //get the width of the object
+        float width = rend.bounds.size.x;
+        float height = rend.bounds.size.y;
+
+        //1% of the time, switch the direction: 
+        int change = Random.Range(0, 100);
+        if (change == 0) {
+            velocity = new Vector3(-velocity.x, -velocity.y, 0);
+            if (velocity.x > 0f) {
+                anim.Play("GhostRight");
+            }
+            else if (velocity.x < 0f) {
+                anim.Play("GhostLeft");
+            } else if (velocity.y > 0f) {
+                anim.Play("GhostUp");
+            }
+            else {
+                anim.Play("GhostDown");
+            }
+        }
+        
+        else if (change == 1) { //1% of the time, switch from horizontal to vertical, or vice-versa: 
+            if (velocity.x != 0f) {
+                velocity = new Vector3(0f, velocity.x, 0f);
+                if(velocity.y > 0f) {
+                    anim.Play("GhostUp");
+                } else if (velocity.y < 0f) {
+                    anim.Play("GhostDown");
+                }
+            } else {
+                velocity = new Vector3(velocity.y, 0f, 0f);
+                if (velocity.x > 0f) {
+                    anim.Play("GhostRight");
+                    print(velocity);
+                }
+                else if (velocity.x < 0f) {
+                    anim.Play("GhostLeft");
+                }
+            }
+        }
+
+        //make sure the obect is inside the borders... if edge is hit reverse direction
+        if ((transform.position.x <= leftBorder + width / 2.0) && velocity.x < 0f) {
+            velocity = new Vector3(1f, 0f, 0f);
+            anim.Play("GhostRight");
+        }
+        if ((transform.position.x >= rightBorder - width / 2.0) && velocity.x > 0f) {
+            velocity = new Vector3(-1f, 0f, 0f);
+            anim.Play("GhostLeft");
+        }
+        if ((transform.position.y <= bottomBorder + height / 2.0) && velocity.y < 0f) {
+            velocity = new Vector3(0f, 1f, 0f);
+            anim.Play("GhostUp");
+        }
+        if ((transform.position.y >= topBorder - height / 2.0) && velocity.y > 0f) {
+            velocity = new Vector3(0f, -1f, 0f);
+            anim.Play("GhostDown");
+        }
+        transform.position = transform.position + velocity * Time.deltaTime * speed;
+    }
+}
