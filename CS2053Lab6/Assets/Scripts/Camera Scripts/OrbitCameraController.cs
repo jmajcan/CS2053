b48afea9c680adf46c08b5bcfb869685ec9ae117
Jacob Majcan <jacob.majcan16@gmail.com>
2020-03-10 18:22:21 -0300

    Finished part 3 of lab

---
+++ CS2053Lab6/Assets/Scripts/Camera Scripts/OrbitCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class OrbitCameraController : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.RotateAround(target.transform.position, Vector3.up, speed * Time.deltaTime);
+        transform.LookAt(target.transform);
+    }
+}
